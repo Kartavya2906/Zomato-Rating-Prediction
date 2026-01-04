15732a9e9795f168ed8d95cefecde0273f43c5ff
Kartavya2906 <kartavyagupta2906@gmail.com>
2026-01-04 18:35:43 +0530

    Fix model loading paths for deployment

--- app/app.py
+++ app/app.py
@@ -5,9 +5,30 @@ import json
 
 app = Flask(__name__)
 
-# Load model & metadata
-model = joblib.load("../model/zomato_rating_model_rmse_0.321_20251229_0230.joblib")
-with open("../model/zomato_rating_model_metadata_20251229_0230.json") as f:
+# # Load model & metadata
+# model = joblib.load("../model/zomato_rating_model_rmse_0.321_20251229_0230.joblib")
+# with open("../model/zomato_rating_model_metadata_20251229_0230.json") as f:
+#     meta = json.load(f)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "..",
+    "model",
+    "zomato_rating_model_rmse_0.321_20251229_0230.joblib"
+)
+
+META_PATH = os.path.join(
+    BASE_DIR,
+    "..",
+    "model",
+    "zomato_rating_model_metadata_20251229_0230.json"
+)
+
+model = joblib.load(MODEL_PATH)
+
+with open(META_PATH, "r") as f:
     meta = json.load(f)
 
 feature_order = meta["features"]
